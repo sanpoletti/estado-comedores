3c892b0005050c0d0a910ff6dadf0930da91ff9d
Sandra Poletti <spoletti@buenosaires.gob.ar>
2026-01-28 14:43:23 -0300

    Generación persistente de Swagger docs

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -39,7 +39,8 @@ return [
                  * Set this to `json` or `yaml` to determine which documentation file to use in UI
                  */
                 'format_to_use_for_docs' => env('L5_FORMAT_TO_USE_FOR_DOCS', 'json'),
-
+                
+                'docs' => storage_path('api-docs'),
                 /*
                  * Absolute paths to directory containing the swagger annotations are stored.
                  */
